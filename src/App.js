21ea0d8249a1d38b0bc24f5962a1c58a8d281bc3
Erick Ayllon <erickayllon@gmail.com>
2022-04-20 18:16:17 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import LoginButton from "./Components/LoginButton";
+import LogOutButton from "./Components/LogOutButton";
+import Profile from "./Components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
-      <h1>Aplication</h1>
-      <LoginButton />
+      <h1>Aplicación</h1>
+      {isAuthenticated ? <LogOutButton /> : <LoginButton />}
+      <Profile />
     </div>
   );
 }
