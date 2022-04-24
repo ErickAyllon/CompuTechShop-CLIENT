28cfff21fe4822e77a7b7bb7de10bc5cb0dcc360
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-24 14:37:42 -0300

    mejorando

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -5,12 +5,20 @@ import Profile from '../Profile'
 import { useAuth0 } from "@auth0/auth0-react";
 import styles from './NavBar.module.css'
 import SearchBar from '../SearchBar/SearchBar';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   const { isAuthenticated } = useAuth0();
   return (
     <nav className={styles.NavBar}>
-      <SearchBar />
+      <div className={styles.logo}>
+        <Link to="/">
+          
+        </Link>
+      </div>
+      <div className={styles.searchBarCall}>
+        <SearchBar />
+      </div>
       <div className={styles.auth0}>
         {isAuthenticated ? <LogOutButton /> : <LoginButton />}
         <Profile />
