695fffd81be7b2feab3d80a6f58f89d7a983262b
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-26 16:02:25 -0300

    Categories-Routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import Laptops from './Components/Categories/Laptops/Laptops.jsx'
+import Monitors from "./Components/Categories/Monitors/Monitors";
+import Headsets from "./Components/Categories/Headsets/Headsets";
+import Keyboards from "./Components/Categories/Keyboards/Keyboards";
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
         <Route path='/product' element={<CreateProduct/>}/>
         <Route path='/profile' element={<Profile/>}/> */}
         <Route path='/laptops' element={<Laptops/>}/>
+        <Route path='/monitors' element={<Monitors/>}/>
+        <Route path='/headsets' element={<Headsets/>}/>
+        <Route path='/keyboards' element={<Keyboards/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/Categories/Headsets/Headsets.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Headsets.module.css'
+
+function Headsets() {
+  return (
+    <div className={styles.headsets}>
+      This is path="/headsets"
+    </div>
+  )
+}
+
+export default Headsets
