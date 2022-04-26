4a178bbd3295375f827b5b49893968ed0a5a2a5c
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-26 10:44:31 -0300

    OK

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,6 +15,14 @@ function Footer() {
             <p>Work with us</p>
             <p>About</p>
           </div>
+          <div className={styles.socialMedia}>
+            <a href="https://www.instagram.com/" target="_blank">
+              <img src="https://img.icons8.com/nolan/64/instagram-new.png"/>
+            </a>
+            <a href="https://www.facebook.com/" target="_blank">
+              <img src="https://img.icons8.com/nolan/64/facebook-new.png"/> 
+            </a>
+          </div>
         </div>
     </div>
   )
