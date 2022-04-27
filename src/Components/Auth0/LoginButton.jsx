03c5acfe7ac65f2c3c825cb323d79d1a3fa37cfa
Erick Ayllon <erickayllon@gmail.com>
2022-04-27 16:31:30 -0300

    actualizando deendencias

--- src/Components/Auth0/LoginButton.jsx
+++ src/Components/Auth0/LoginButton.jsx
@@ -1,13 +1,49 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import styles from './LoginButton.module.css'
+import styles from "./LoginButton.module.css";
 
+// function handleSubmit(e) {
+//   e.preventDefault();
+
+//   if (Object.values(errors).length > 0) {
+//     alert("Por favor complete con la información requerida");
+//   } else if (
+//     input.name === "" &&
+//     input.summary === "" &&
+//     input.spoonacularScore === "" &&
+//     input.healthScore === "" &&
+//     input.steps === "" &&
+//     !input.dietTypes.length
+//   ) {
+//     alert("Por favor complete el formulario");
+//   } else {
+//     dispatch(addRecipe(input));
+//     alert("Nueva receta agregada correctamente");
+//     setInput({
+//       name: "",
+//       summary: "",
+//       spoonacularScore: "",
+//       healthScore: "",
+//       steps: "",
+//       dietTypes: [],
+//       image: "",
+//     });
+//     history.push("/home");
+//   }
+// }
 function LoginButton() {
   const { loginWithRedirect } = useAuth0();
   return (
-    <button className={styles.logInButton} onClick={() => {loginWithRedirect()}}>
+    <button
+      className={styles.logInButton}
+      onClick={() => {
+        loginWithRedirect();
+      }}
+      // onSubmit={(e) => handleSubmit(e)}
+    >
       <p>Log in</p>
-    </button>);
+    </button>
+  );
 }
 
 export default LoginButton;
--- src/Components/Auth0/Profile.jsx
+++ src/Components/Auth0/Profile.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import styles from './Profile.module.css'
+import styles from "./Profile.module.css";
 
 export default function Profile() {
   const { user, isAuthenticated } = useAuth0();
-
+  console.log(user);
   return (
     isAuthenticated && (
       <div className={styles.profile}>
-        <img className={styles.profileImg} src={user.picture} alt="profileImg" />
+        <img
+          className={styles.profileImg}
+          src={user.picture}
+          alt="profileImg"
+        />
         <h2>{user.given_name}</h2>
 
         {/* <p>{user.email}</p> */}
