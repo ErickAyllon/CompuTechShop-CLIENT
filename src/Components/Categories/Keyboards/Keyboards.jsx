c65127a67809ebdffd843201b0ae6e12a58e3b4f
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-29 23:49:21 -0300

    Habemus pagination

--- src/Components/Categories/Keyboards/Keyboards.jsx
+++ src/Components/Categories/Keyboards/Keyboards.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import styles from './Keyboards.module.css'
 import Categories from '../Categories'
 import {useDispatch, useSelector} from 'react-redux'
-import { useEffect } from 'react'
 import {filterByCategory} from '../../../Redux/Actions'
 import ProductCard from '../../ProductCard/ProductCard'
 import { Link } from 'react-router-dom';
 import Filter from '../../Filter/Filter'
 import PaginationC from '../../Pagination/PaginationC';
 import Loader from '../../Loader/Loader'
+import { useLocation } from 'react-router-dom';
 
 function Keyboards() {
   const products = useSelector ((state) => state.products)
@@ -16,10 +16,26 @@ function Keyboards() {
   const category = 'Keyboards';
   // const {category} = useParams
 
+  // Pagination Info //
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const page = parseInt(query.get('page') || '1', 10);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 1;
+  const indexLastProduct = currentPage * productsPerPage;
+  const indexFirstProduct = indexLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexFirstProduct, indexLastProduct);
+  const totalPages = Math.ceil(products.length / productsPerPage);
+
   useEffect(() => {
     dispatch(filterByCategory(category));
+    setCurrentPage(page)
   }, [dispatch]);
-   
+
+  const pagination = (pageNumber) => {
+    setCurrentPage(pageNumber);
+}
+ // End Pagination //
   
   return (
     <div className={styles.keyboards}>
@@ -30,7 +46,7 @@ function Keyboards() {
       <div className={styles.productsContainer}>
         <Filter />
         <div className={styles.productsCardsContainer}>
-          {products.map((el) => {
+          {currentProducts.map((el) => {
             return (
                 <ProductCard 
                   name={el.name} 
@@ -45,7 +61,11 @@ function Keyboards() {
           })}
         </div>
       </div>
-        <PaginationC />
+      <PaginationC 
+          category={category}
+          pagination={pagination} 
+          totalPages={totalPages}
+        />
       </>
           :
           <Loader />
--- src/Components/Pagination/PaginationC.jsx
+++ src/Components/Pagination/PaginationC.jsx
@@ -1,66 +1,45 @@
 import * as React from 'react';
-// import Pagination from 'react-bootstrap/Pagination'
 import styles from './PaginationC.module.css'
-import { Link, MemoryRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Pagination from '@mui/material/Pagination';
 import PaginationItem from '@mui/material/PaginationItem';
+import { useEffect } from 'react';
 
-function PaginationC({category}) {
-  // category={category}
-  // productsPerPage={productsPerPage} 
-  // products={products.length} 
-  // pagination={pagination} 
-  // currentPage={currentPage} 
-  // setCurrentPage={setCurrentPage}
+function PaginationC({category, pagination, totalPages  }) {
 
   const location = useLocation();
   const query = new URLSearchParams(location.search);
   const page = parseInt(query.get('page') || '1', 10);
 
-  // const pageNumbers = [];
-  // const [input, setInput] = useState(currentPage)
-  // const max = allPokemons / pokemonsPerPage
-  
-  // for (let i = 1; i <= Math.ceil(max); i++) {
-  //     pageNumbers.push(i)
-  // }
-
-  // function nextPage() {
-  //   setCurrentPage(currentPage + 1);
-  //   setInput(input - 1 );
-  // }
-
-  // function prevPage() {
-  //     setCurrentPage(currentPage - 1);
-  //     setInput(input - 1 );
-  // }
+  useEffect(() => {
+    pagination(page)
+  })
 
   return (
     <div  className={styles.pagination}>
-    <Pagination
-      page={page}
-      count={5}
-      renderItem={(item) => (
-        <PaginationItem
-          component={Link}
-          to={`/${category}${item.page === 1 ? '' : `?page=${item.page}`}`}
-          {...item}
-        />
-      )}
-    />
-
-    {/* <div style={{  padding: 30 }}>
-      <Pagination style={{ width: '100%', justifyContent: 'center' }}>
-        <Pagination.Prev />
-        <Pagination.Ellipsis />
-        <Pagination.Item>{3}</Pagination.Item>
-        <Pagination.Item>{4}</Pagination.Item>
-        <Pagination.Item>{5}</Pagination.Item>
-        <Pagination.Ellipsis />
-        <Pagination.Next />
-      </Pagination>
-    </div> */}
-    
+      <Pagination
+        color="primary"
+        size="medium"
+        variant="outlined"
+        shape="circular"
+        // shape="rounded"
+        // siblingCount="2"
+        page={page}
+        count={totalPages}
+        sx={{
+          color: 'white',
+          "& .MuiPaginationItem-root": {
+            color:"white",
+          },
+        }}
+        renderItem={(item) => (
+          <PaginationItem
+            component={Link}
+            to={`/${category}${item.page === 1 ? '' : `?page=${item.page}`}`}
+            {...item}
+          />
+        )}
+      />
     </div>
   )
 }
