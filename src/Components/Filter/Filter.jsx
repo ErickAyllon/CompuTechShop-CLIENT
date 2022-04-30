d152d1a96b6f75570d10f356c32001bc208d9d65
Martin <raschinsky83@gmail.com>
2022-04-30 18:42:06 -0300

    vamoviendo

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
+
 import styles from './Filter.module.css'
-
-
+import React, { useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { filterByBrandCategories} from '../../Redux/Actions'
 
 function Filter() {
+
+  const allProducts = useSelector((state) => state.allProducts)
+  const products = useSelector ((state) => state.products)
+  const productsDetail = useSelector((state) => state.productsDetail)
+  const dispatch = useDispatch();
+  
+  function handleFilterByBrandCategories(e){
+    e.preventDefault()
+    dispatch(filterByBrandCategories(e.target.value))
+  }
+  
+  
+    const setBrand = new Set(); 
+  
+  const unicBrand = products.reduce((acc, marca) => {
+    if (!setBrand.has(marca.brand)){
+      setBrand.add(marca.brand, marca)
+      acc.push(marca)
+    }
+    return acc;
+  },[]);
+  
+  const brandMap = unicBrand.map((el)=>el.brand)
+  
 
 
   return (
     
     <div className={styles.filter}>
         <div className={styles.filterContainer}>
+        <select onChange={e=>handleFilterByBrandCategories(e)}>
+          <option value= "all">all</option>
+          {
+                  brandMap?.map((t) => 
+                  (<option value={t} key={t}> {t} </option> 
+                  ))}
         
-          
-          
-          <p>Etc</p>
+        </select>
         </div>
     </div>
   )
