9508feff75f312566518e4108e58180e01b74dcb
Erick Ayllon <erickayllon@gmail.com>
2022-05-11 16:34:46 -0300

    cambios en dotenv

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { Provider } from "react-redux";
 import store from "./Redux/Store/index.js";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
