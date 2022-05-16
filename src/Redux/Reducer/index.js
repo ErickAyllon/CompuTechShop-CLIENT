c9606a26eca763731121e1b67863ba98635820bc
Erick Ayllon <erickayllon@gmail.com>
2022-05-16 12:27:55 -0300

    Update index.js

--- src/Redux/Reducer/index.js
+++ src/Redux/Reducer/index.js
@@ -33,6 +33,10 @@ const initialState = {
 };
 
 function rootReducer(state = initialState, action) {
+  function toNumber(something) {
+    let result = parseInt(something.replace(".", ""));
+    return Number(result);
+  }
   switch (action.type) {
     case "GET_PRODUCTS":
       // let ordered = action.payload.sort(
@@ -126,10 +130,10 @@ function rootReducer(state = initialState, action) {
         productsNotPriceChangeable: filtered,
       };
     case "FILTER_BY_PRICE":
-      function toNumber(something) {
-        let result = parseInt(something.replace(".", ""));
-        return Number(result);
-      }
+      // function toNumber(something) {
+      //   let result = parseInt(something.replace(".", ""));
+      //   return Number(result);
+      // }
       let productsFiltered = state.productsNotPriceChangeable;
       let min = action.payload.min;
       let max = action.payload.max;
