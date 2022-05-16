2bdb24d084b520c7666fd4717590e34bbefaea64
Erick Ayllon <erickayllon@gmail.com>
2022-05-16 18:29:37 -0300

    cambios en develop

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 import { getUser, authenticate } from "../../Redux/Actions/index.js";
@@ -8,26 +8,26 @@ import styles from './Welcome.module.css'
 import logo3 from '../../Images/logo3.png'
 
 function Welcome() {
-  const { user, isAuthenticated } = useAuth0();
+  const { user } = useAuth0();
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users)
   const auth0Email = user?.email
   const userLogged = users?.length > 0 ? users?.find(e => (e.email === auth0Email)) : false;
 
-  useEffect(()  => {
+  useEffect(() => {
     dispatch(getUser())
     dispatch(authenticate(userLogged))
 
   }, [dispatch])
   return (
     <div className={styles.welcome}>
-        <div className={styles.welcomeContainer}>
-            <h1>Welcome to CompuTechShop!</h1>
-            <img src={logo3} alt="logo"/>
-            <Link to='/'>
-                <Button variant='outlined'>Go Shopping</Button>
-            </Link>
-        </div>
+      <div className={styles.welcomeContainer}>
+        <h1>Welcome to CompuTechShop!</h1>
+        <img src={logo3} alt="logo" />
+        <Link to='/'>
+          <Button variant='outlined'>Go Shopping</Button>
+        </Link>
+      </div>
     </div>
   )
 }
